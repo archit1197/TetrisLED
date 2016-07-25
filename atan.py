71f48a16616cc751a712ac7bcf6540a1a750c9de
Archit Gupta <archit1197@gmail.com>
2016-07-25 19:18:23 +0530

    Add files via upload
    points calculator and fixed error of bottom 0s not displaying

--- atan.py
+++ atan.py
@@ -3,14 +3,15 @@ import os
 import random
 import signal
 import sys
+import pygame
 
 board_x=8
 board_y=8
 
 matrix=[[0 for i in range(0,board_x)] for j in range(0,board_y)]
 matrix_2=[[0 for i in range(0,board_x)] for j in range(0,board_y)]
-
-height=[8 for i in range(board_x)]
+dead_list=[]				#it will store all those passive objects which are now at the bottom, can't be moved further. 
+height=[board_x for i in range(board_y)]
 	
 """
 for i in range(0,8):
@@ -22,10 +23,11 @@ for i in range(0,8):
 		print matrix[j]
 	time.sleep(2)
 """
-def print_matrix():
+def print_matrix(matri):
+	matrix_2=matri
 	matrix=[[0 for i in range(0,board_x)] for j in range(0,board_y)]
 	setp(matrix,thing)
-	for i in range(8):
+	for i in range(board_x):
 		print matrix[i]
 	print "--------"
 	
@@ -34,13 +36,15 @@ def setp(matrix,object):
 	
 	try:
 		for i in range(len(object.listpoints)):
-			if object.listpoints[i][0]<8 and object.listpoints[i][1]<8:
+			if object.listpoints[i][0]<board_x and object.listpoints[i][1]<board_y:
 				matrix[object.listpoints[i][0]][object.listpoints[i][1]]=1	
 
-		for j in range(8):
-			if (height[j]<8):
-				for k in range(height[j],8):
-						matrix[k][j]=1
+		for j in range(board_y):
+			if (height[j]<board_x):
+				for k in range(height[j],board_x):
+					for i in dead_list:
+						if i.check(k,j)==1:
+							matrix[k][j]=1
 	except:
 		abcd=0
 	return
@@ -54,6 +58,7 @@ class piece:
 
 
 	def left(self):
+
 		for i in range(0,len(self.listpoints)):
 			if self.listpoints[i][1]==0:
 				return False	
@@ -102,26 +107,38 @@ class piece:
 			self.listpoints[i][0]+=1	
 		return True
 		
-
+	def check(self,i,j):
+		ch=0
+		for k in range(len(self.listpoints)):
+			if self.listpoints[k][0]==i and self.listpoints[k][1]==j:
+				ch=1
+		return ch 
+	
 	def rotate(self):
 		if self.shape=='l':
 			if(self.orientation%4==0):
+				if (self.listpoints[2][0]+1<board_x and self.listpoints[2][1]-1>-1 and self.listpoints[0][0]-1>-1 and self.listpoints[0][1]+1<board_y):
+					if(matrix[self.listpoints[2][0]+1][self.listpoints[2][1]-1]==0 and matrix[self.listpoints[0][0]-1][self.listpoints[0][1]+1]==0):
+						self.listpoints[2][0]+=1
+						self.listpoints[2][1]-=1
+						self.listpoints[0][0]-=1
+						self.listpoints[0][1]+=1
+					else:
+						return False
+				else:
+					return False
 
-				if(matrix[self.listpoints[2][0]+1][self.listpoints[2][1]-1]==0 and matrix[self.listpoints[0][0]-1][self.listpoints[0][1]+1]==0):
-					self.listpoints[2][0]+=1
-					self.listpoints[2][1]-=1
-					self.listpoints[0][0]-=1
-					self.listpoints[0][1]+=1
-				else:
-					return False
 			elif(self.orientation%4==1):
-				if(matrix[self.listpoints[2][0]-1][self.listpoints[2][1]+1]==0 and matrix[self.listpoints[0][0]+1][self.listpoints[0][1]-1]==0):
-					self.listpoints[2][0]-=1
-					self.listpoints[2][1]+=1
-					self.listpoints[0][0]+=1
-					self.listpoints[0][1]-=1
+				if (self.listpoints[2][0]-1>-1 and self.listpoints[2][1]+1<board_y and self.listpoints[0][0]+1<board_x and self.listpoints[0][1]-1>-1):
+					if(matrix[self.listpoints[2][0]-1][self.listpoints[2][1]+1]==0 and matrix[self.listpoints[0][0]+1][self.listpoints[0][1]-1]==0):
+						self.listpoints[2][0]-=1
+						self.listpoints[2][1]+=1
+						self.listpoints[0][0]+=1
+						self.listpoints[0][1]-=1
+					else:
+						return False
 				else:
-					return False
+					return False 		
 			self.orientation=1-self.orientation
 
 		elif self.shape=='s':
@@ -129,43 +146,56 @@ class piece:
 
 		if self.shape=='t':
 			if(self.orientation%4==0):
-				if(matrix[self.listpoints[2][0]+1][self.listpoints[2][1]-1]==0 and matrix[self.listpoints[0][0]-1][self.listpoints[0][1]+1]==0 and matrix[self.listpoints[3][0]][self.listpoints[3][1]-2]==0):
-					self.listpoints[2][0]+=1
-					self.listpoints[2][1]-=1
-					self.listpoints[0][0]-=1
-					self.listpoints[0][1]+=1
-					self.listpoints[3][1]-=2
+				if (self.listpoints[2][0]+1<board_x and self.listpoints[2][1]-1>-1 and self.listpoints[0][0]-1>-1 and self.listpoints[0][1]+1<board_y and self.listpoints[3][1]-2>-1):
+					if(matrix[self.listpoints[2][0]+1][self.listpoints[2][1]-1]==0 and matrix[self.listpoints[0][0]-1][self.listpoints[0][1]+1]==0 and matrix[self.listpoints[3][0]][self.listpoints[3][1]-2]==0):
+						self.listpoints[2][0]+=1
+						self.listpoints[2][1]-=1
+						self.listpoints[0][0]-=1
+						self.listpoints[0][1]+=1
+						self.listpoints[3][1]-=2
+					else:
+						return False
 				else:
 					return False
 
 			elif(self.orientation%4==1):
-				if(matrix[self.listpoints[2][0]-1][self.listpoints[2][1]+1]==0 and matrix[self.listpoints[0][0]+1][self.listpoints[0][1]-1]==0 and matrix[self.listpoints[3][0]-2][self.listpoints[3][1]]==0):
-					self.listpoints[2][0]-=1
-					self.listpoints[2][1]+=1
-					self.listpoints[0][0]+=1
-					self.listpoints[0][1]-=1
-					self.listpoints[3][0]-=2
+				if (self.listpoints[2][0]-1>-1 and self.listpoints[2][1]+1<board_y and self.listpoints[0][0]+1<board_x and self.listpoints[0][1]-1>-1 and self.listpoints[3][0]-2>-1):
+					if(matrix[self.listpoints[2][0]-1][self.listpoints[2][1]+1]==0 and matrix[self.listpoints[0][0]+1][self.listpoints[0][1]-1]==0 and matrix[self.listpoints[3][0]-2][self.listpoints[3][1]]==0):
+						self.listpoints[2][0]-=1
+						self.listpoints[2][1]+=1
+						self.listpoints[0][0]+=1
+						self.listpoints[0][1]-=1
+						self.listpoints[3][0]-=2
+					else:
+						return False
 				else:
-					return False
+					return False	
 			elif(self.orientation%4==2):
-				if(matrix[self.listpoints[2][0]+1][self.listpoints[2][1]-1]==0 and matrix[self.listpoints[0][0]-1][self.listpoints[0][1]+1]==0 and matrix[self.listpoints[3][0]][self.listpoints[3][1]+2]==0):
-					self.listpoints[2][0]+=1
-					self.listpoints[2][1]-=1
-					self.listpoints[0][0]-=1
-					self.listpoints[0][1]+=1
-					self.listpoints[3][1]+=2
+				if (self.listpoints[2][0]+1<board_x and self.listpoints[2][1]-1>-1 and self.listpoints[0][0]-1>-1 and self.listpoints[0][1]+1<board_y and self.listpoints[3][1]+2<board_y):
+					if(matrix[self.listpoints[2][0]+1][self.listpoints[2][1]-1]==0 and matrix[self.listpoints[0][0]-1][self.listpoints[0][1]+1]==0 and matrix[self.listpoints[3][0]][self.listpoints[3][1]+2]==0):
+						self.listpoints[2][0]+=1
+						self.listpoints[2][1]-=1
+						self.listpoints[0][0]-=1
+						self.listpoints[0][1]+=1
+						self.listpoints[3][1]+=2
+					else:
+						return False
 				else:
 					return False
 
 			elif(self.orientation%4==3):
-				if(matrix[self.listpoints[2][0]-1][self.listpoints[2][1]+1]==0 and matrix[self.listpoints[0][0]+1][self.listpoints[0][1]-1]==0 and matrix[self.listpoints[3][0]+2][self.listpoints[3][1]]==0):
-					self.listpoints[2][0]-=1
-					self.listpoints[2][1]+=1
-					self.listpoints[0][0]+=1
-					self.listpoints[0][1]-=1
-					self.listpoints[3][0]+=2
+				if (self.listpoints[2][0]-1>-1 and self.listpoints[2][1]+1<board_y and self.listpoints[0][0]+1<board_x and self.listpoints[0][1]-1>-1 and self.listpoints[3][0]+2<board_x):
+					if(matrix[self.listpoints[2][0]-1][self.listpoints[2][1]+1]==0 and matrix[self.listpoints[0][0]+1][self.listpoints[0][1]-1]==0 and matrix[self.listpoints[3][0]+2][self.listpoints[3][1]]==0):
+						self.listpoints[2][0]-=1
+						self.listpoints[2][1]+=1
+						self.listpoints[0][0]+=1
+						self.listpoints[0][1]-=1
+						self.listpoints[3][0]+=2
+					else:
+						return False
 				else:
-					return False
+					return False			
+
 			self.orientation+=1
 
 		return True
@@ -210,12 +240,12 @@ def choose_object():
 
 thing=choose_object()
 
-
+count=0
 while(1):
 	y=True
 	
-	
-	print_matrix()
+	ends=0
+	print_matrix(matrix)
 	#choice=input("Enter Choice: ")	
 	#choice=sys.stdin.read(1)
 	def signal_handler(signum, frame):
@@ -239,11 +269,27 @@ while(1):
 		w=thing.rotate()	
 
 
-	print_matrix()
+	print_matrix(matrix)
 	os.system('clear')
 
 
 	if y==False:
+		dead_list.append(thing)
 		thing=choose_object()
+
+
+	count+=1
+	for i in range(len(height)):
+		if height[i]==0:
+			ends=1
+			break
+	
+	if ends==1:
+		break
+
+print "Your game has ended."
+print "Your final score:",count
+print ".....Thank you......"
+
 
 		
